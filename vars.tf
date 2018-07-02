c05583e9c31279ef6bd90d031947a639114fabbc
daringanitch <dganitch@gmail.com>
2018-07-02 16:39:37 -0700

    Update vars.tf
    more variables

--- vars.tf
+++ vars.tf
@@ -1,14 +1,26 @@
 variable "AWS_REGION" {
-  default = "us-west-1"
+  default = "us-west-2"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "mykey2"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "mykey2.pub"
 }
-variable "iam_role" {
+variable "web_iam_role" {
   default = "s3-bucket-role-09"
+}
+variable "s3_iam_role_policy" {
+  default = "s3-role-policy-09"
+}
+variable "web_instance_profile" {
+  default = "profile-09"
+}
+variable "web_iam_role_policy" {
+  default = "policy-09"
+}
+variable "s3_iam_role" {
+  default = "s3-role-09"
 }
 variable "AMIS" {
   type = "map"
