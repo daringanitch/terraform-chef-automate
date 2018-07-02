3132521a50632f6747a2191a9e8383971a9977ef
daringanitch <dganitch@gmail.com>
2018-07-02 16:12:38 -0700

    Update vars.tf
    new variable for iam role

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "us-west-2"
+  default = "us-west-1"
 }
 variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
@@ -7,10 +7,14 @@ variable "PATH_TO_PRIVATE_KEY" {
 variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
 }
+variable "iam_role" {
+  default = "s3-bucket-role-09"
+}
 variable "AMIS" {
   type = "map"
   default = {
     us-east-1 = "ami-e251209a"
     us-west-2 = "ami-e251209a"
+    us-west-1 = "ami-e251209a"
    }
 }
