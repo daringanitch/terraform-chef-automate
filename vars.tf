8676cbb883acc838e6094a5114d0129fcf95c3c0
daringanitch <dganitch@gmail.com>
2018-07-05 15:10:16 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -7,21 +7,6 @@ variable "PATH_TO_PRIVATE_KEY" {
 variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey2.pub"
 }
-variable "web_iam_role" {
-  default = "s3-bucket-role-09"
-}
-variable "s3_iam_role_policy" {
-  default = "s3-role-policy-09"
-}
-variable "web_instance_profile" {
-  default = "profile-09"
-}
-variable "web_iam_role_policy" {
-  default = "policy-09"
-}
-variable "s3_iam_role" {
-  default = "s3-role-09"
-}
 variable "AMIS" {
   type = "map"
   default = {
