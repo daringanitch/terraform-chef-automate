362d17793c291ae30456276e021e73c46daee16f
daringanitch <dganitch@gmail.com>
2018-07-05 15:03:01 -0700

    Update vpc.tf
    fix vpc issues

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
     enable_dns_hostnames = "true"
     enable_classiclink = "false"
     tags {
-        Name = "main-vpc"
+        Name = "main"
     }
 }
 
@@ -14,9 +14,9 @@ resource "aws_vpc" "main" {
 # Subnets
 resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.14.0/24"
+    cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "us-west-2a"
+    availability_zone = "eu-west-1a"
 
     tags {
         Name = "main-public-1"
@@ -24,9 +24,9 @@ resource "aws_subnet" "main-public-1" {
 }
 resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.15.0/24"
+    cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "us-west-2b"
+    availability_zone = "eu-west-1b"
 
     tags {
         Name = "main-public-2"
@@ -34,9 +34,9 @@ resource "aws_subnet" "main-public-2" {
 }
 resource "aws_subnet" "main-public-3" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.16.0/24"
+    cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "us-west-2c"
+    availability_zone = "eu-west-1c"
 
     tags {
         Name = "main-public-3"
@@ -44,9 +44,9 @@ resource "aws_subnet" "main-public-3" {
 }
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.17.0/24"
+    cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "us-west-2a"
+    availability_zone = "eu-west-1a"
 
     tags {
         Name = "main-private-1"
@@ -54,9 +54,9 @@ resource "aws_subnet" "main-private-1" {
 }
 resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.18.0/24"
+    cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "us-west-2b"
+    availability_zone = "eu-west-1b"
 
     tags {
         Name = "main-private-2"
@@ -64,9 +64,9 @@ resource "aws_subnet" "main-private-2" {
 }
 resource "aws_subnet" "main-private-3" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.19.0/24"
+    cidr_block = "10.0.6.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "us-west-2c"
+    availability_zone = "eu-west-1c"
 
     tags {
         Name = "main-private-3"
@@ -78,7 +78,7 @@ resource "aws_internet_gateway" "main-gw" {
     vpc_id = "${aws_vpc.main.id}"
 
     tags {
-        Name = "main-gw"
+        Name = "main"
     }
 }
 
@@ -107,6 +107,30 @@ resource "aws_route_table_association" "main-public-2-a" {
 resource "aws_route_table_association" "main-public-3-a" {
     subnet_id = "${aws_subnet.main-public-3.id}"
     route_table_id = "${aws_route_table.main-public.id}"
+}
+resource "aws_route_table" "main-private" {
+    vpc_id = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = "${aws_nat_gateway.nat-gw.id}"
+    }
+
+    tags {
+        Name = "main-private-1"
+    }
+}
+# route associations private
+resource "aws_route_table_association" "main-private-1-a" {
+    subnet_id = "${aws_subnet.main-private-1.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
+}
+resource "aws_route_table_association" "main-private-2-a" {
+    subnet_id = "${aws_subnet.main-private-2.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
+}
+resource "aws_route_table_association" "main-private-3-a" {
+    subnet_id = "${aws_subnet.main-private-3.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
 }
 # nat gw
 resource "aws_eip" "nat" {
