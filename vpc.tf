4ebc3c2986cecbcbefee0b03a2d575b400d7f53e
daringanitch <dganitch@gmail.com>
2018-07-05 10:01:38 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -108,3 +108,12 @@ resource "aws_route_table_association" "main-public-3-a" {
     subnet_id = "${aws_subnet.main-public-3.id}"
     route_table_id = "${aws_route_table.main-public.id}"
 }
+# nat gw
+resource "aws_eip" "nat" {
+  vpc      = true
+}
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id = "${aws_subnet.main-public-1.id}"
+  depends_on = ["aws_internet_gateway.main-gw"]
+}
