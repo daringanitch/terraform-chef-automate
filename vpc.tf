2e63e307c9bcabcfd7302ccac02f3930f0a88c6d
daringanitch <dganitch@gmail.com>
2018-07-05 15:06:14 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1a"
+    availability_zone = "us-west-1a"
 
     tags {
         Name = "main-public-1"
@@ -26,7 +26,7 @@ resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1b"
+    availability_zone = "us-west-1b"
 
     tags {
         Name = "main-public-2"
@@ -46,7 +46,7 @@ resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1a"
+    availability_zone = "us-west-1a"
 
     tags {
         Name = "main-private-1"
@@ -56,7 +56,7 @@ resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1b"
+    availability_zone = "us-west-1b"
 
     tags {
         Name = "main-private-2"
@@ -66,7 +66,7 @@ resource "aws_subnet" "main-private-3" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.6.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1c"
+    availability_zone = "us-west-1c"
 
     tags {
         Name = "main-private-3"
