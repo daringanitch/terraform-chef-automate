1f85540c30dfa546ed6e0a810f4229f22ee81e4d
daringanitch <dganitch@gmail.com>
2018-07-06 09:37:27 -0700

    Update role.tf

--- role.tf
+++ role.tf
@@ -1,47 +1,7 @@
-# Create an IAM role for the  Servers.
-resource "aws_iam_role" "s3_iam_role" {
-    name = "s3_iam_role"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_instance_profile" "s3_instance_profile" {
-    name = "s3_instance_profile"
-    roles = ["web_iam_role"]
-}
-
-resource "aws_iam_role_policy" "s3_iam_role_policy" {
-  name = "s3_iam_role_policy"
-  role = "${aws_iam_role.s3_iam_role.id}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
 # Create an IAM role for the Web Servers.
-resource "aws_iam_role" "web_iam_role" {
-    name = "web_iam_role"
-    assume_role_policy = <<EOF
+resource "aws_iam_role" "tr_s3_iam_role" {
+    name = "${var.aws_iam_role}"
+        assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -58,14 +18,14 @@ resource "aws_iam_role" "web_iam_role" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "web_instance_profile" {
-    name = "web_instance_profile"
-    roles = ["web_iam_role"]
+resource "aws_iam_instance_profile" "tr_s3_instance_profile" {
+    name = "${var.aws_iam_instance_profile}"
+    roles = ["${var.aws_iam_role}"]
 }
 
-resource "aws_iam_role_policy" "web_iam_role_policy" {
-  name = "web_iam_role_policy"
-  role = "${aws_iam_role.web_iam_role.id}"
+  resource "aws_iam_role_policy" "tr_s3_iam_role_policy" {
+  name = "${var.aws_iam_role_policy}"
+  role = "${aws_iam_role.tr_s3_iam_role.id}"
   policy = <<EOF
 {
   "Version": "2012-10-17",
