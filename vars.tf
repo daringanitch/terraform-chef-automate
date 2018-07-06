c501c334fdd85801beb2f3e4388bff1d596e9419
daringanitch <dganitch@gmail.com>
2018-07-06 09:38:48 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,13 +2,22 @@ variable "AWS_REGION" {
   default = "us-west-2"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey2"
+  default = "mykey3"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey2.pub"
+  default = "mykey3.pub"
 }
 variable "s3_bucket_name" {
   default = "ist-chef-license"
+}
+variable "aws_iam_role" {
+  default = "tr_s3_iam_role"
+}
+variable "aws_iam_instance_profile" {
+  default = "tr_s3_instance_profile"
+}
+variable "aws_iam_role_policy" {
+  default = "tr_s3_iam_role_policy"
 }
 variable "AMIS" {
   type = "map"
